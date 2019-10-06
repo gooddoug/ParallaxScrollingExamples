6b46e711a9fa57182345720199a7dde75b2539be
Doug Whitmore <dougwhitmore@mac.com>
2019-10-06 09:25:35 -0700

    Update to allow moving on macOS

--- ParallaxScrollingBackgroundOSX/GameScene.swift
+++ ParallaxScrollingBackgroundOSX/GameScene.swift
@@ -14,7 +14,7 @@ class GameScene: SKScene {
     enum Movement {
         case forward, back, none
         
-        var offset: CGFloat {
+        var xOffset: CGFloat {
             switch self {
             case .forward:
                 return 2.0
@@ -25,39 +25,116 @@ class GameScene: SKScene {
             }
         }
         
+        var shipDirection: ShipDirection? {
+            switch self {
+            case .forward:
+                return ShipDirection.forward
+            case .back:
+                return ShipDirection.back
+            default:
+                return nil
+            }
+        }
     }
     
-    var movement = Movement.forward
+    enum ShipDirection {
+        case forward, back
+        
+        var shipRotationValue: CGFloat {
+            switch self {
+            case .forward:
+                return -CGFloat.pi / 2
+            case .back:
+                return CGFloat.pi / 2
+            }
+        }
+        
+        func shipRotation(forYMovement yMovement: YMovement) -> CGFloat {
+            return shipRotationValue + (upDownRotationOffset * yMovement.shipRotationOffset)
+        }
+        
+        var upDownRotationOffset: CGFloat {
+            switch self {
+            case .forward:
+                return 1
+            case .back:
+                return -1
+            }
+        }
+    }
+    
+    enum YMovement {
+        case up, down, none
+        
+        var yOffset: CGFloat {
+            switch self {
+            case .up:
+                return 2.0
+            case .down:
+                return -2.0
+            case .none:
+                return 0.0
+            }
+        }
+        
+        var shipRotationOffset: CGFloat {
+            switch self {
+            case .up:
+                return CGFloat.pi / 20
+            case .down:
+                return -CGFloat.pi / 20
+            case .none:
+                return 0.0
+            }
+        }
+    }
+    
+    var movement = Movement.forward {
+        didSet {
+            if let direction = movement.shipDirection, oldValue != movement {
+                shipDirection = direction
+            }
+        }
+    }
+    var yMovement = YMovement.none {
+        didSet {
+            if oldValue != yMovement {
+                ship.zRotation = shipDirection.shipRotation(forYMovement: yMovement)
+            }
+        }
+    }
+    var shipDirection = ShipDirection.forward
+    let ship = SKSpriteNode(imageNamed:"Spaceship")
     
     override func didMove(to view: SKView) {
         background = SimpleParallaxBackground(viewSize: self.frame.size, foreground: Image(named: "mountain_fore")!, background: Image(named: "mountain_bkgd")!)
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        self.addChild(myLabel)
         
         background.setup(inScene: self)
         
         let moveBackground = SKAction.customAction(withDuration: 1.0) { _, _ in
-            self.background.xOffset = self.background.xOffset + self.movement.offset
+            self.background.xOffset = self.background.xOffset + self.movement.xOffset
         }
         self.run(SKAction.repeatForever(moveBackground))
+        
+        ship.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        ship.setScale(0.15)
+        ship.zRotation = shipDirection.shipRotation(forYMovement: yMovement)
+        self.addChild(ship)
     }
 
     override func mouseDown(with event: NSEvent) {
         /* Called when a mouse click occurs */
         
-        let location = event.location(in: self)
-        
-        let sprite = SKSpriteNode(imageNamed:"Spaceship")
-        sprite.position = location;
-        sprite.setScale(0.5)
-        
-        let action = SKAction.rotate(byAngle: CGFloat.pi, duration:1)
-        sprite.run(SKAction.repeatForever(action))
-        
-        self.addChild(sprite)
+//        let location = event.location(in: self)
+//
+//        let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//        sprite.position = location;
+//        sprite.setScale(0.5)
+//
+//        let action = SKAction.rotate(byAngle: CGFloat.pi, duration:1)
+//        sprite.run(SKAction.repeatForever(action))
+//
+//        self.addChild(sprite)
     }
     
     
@@ -70,14 +147,22 @@ class GameScene: SKScene {
             switch oneChar {
             case "a":
                 movement = Movement.back
+                ship.zRotation = shipDirection.shipRotation(forYMovement: yMovement)
                 break
             case "d":
                 movement = Movement.forward
+                ship.zRotation = shipDirection.shipRotation(forYMovement: yMovement)
+                break
+            case "s":
+                yMovement = YMovement.down
+                break
+            case "w":
+                yMovement = YMovement.up
                 break
             default:
                 super.keyDown(with: event)
             }
-            
+            //
         }
     }
     
@@ -92,6 +177,12 @@ class GameScene: SKScene {
             case "d":
                 movement = Movement.none
                 break
+            case "s":
+                yMovement = YMovement.none
+                break
+            case "w":
+                yMovement = YMovement.none
+                break
             default:
                 super.keyUp(with: event)
             }
@@ -101,6 +192,13 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        let currentPosition = ship.position
+        let max = self.frame.size.height
+        let newY = currentPosition.y + yMovement.yOffset
+        if newY < max && newY > 50 {
+            let newPosition = CGPoint(x: currentPosition.x, y: newY)
+            ship.position = newPosition
+        }
     }
     
     var shouldAutoscroll = true {
